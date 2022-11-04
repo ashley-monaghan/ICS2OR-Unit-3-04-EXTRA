8114ebb13edadfc13f8888268229db7ed6679501
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-04 15:26:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+// Created by: Ashley Monaghan
+// Created on: Nov 2022
+//
+//This program converts Fahrenheit to Celsius
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double Fahrenheit ;
+        double celsius;
+
+        Console.WriteLine("This program converts Fahrenheit to Celsius.");
+        Console.WriteLine("");
+
+        Console.Write("Enter Fahrenheit (°F): ");
+        Fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+        celsius = (Fahrenheit - 32) * 5/9
+;
+
+        Console.WriteLine("");
+        Console.WriteLine("The celsius is: " + celsius.ToString("0.00") + " °C ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
